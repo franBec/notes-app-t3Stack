3bc4351467f96494b147fb562e1cd92bad4de235
Fran Bec <81979771+franBec@users.noreply.github.com>
2022-08-31 11:35:10 -0300

    Fixed broken pagination when F5

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -6,17 +6,27 @@ import Footer from './footer'
 import LoadingFullScreen from '../utils/loading/loadingFullScreen'
 
 import { useLoading } from '../../zustand/loadingStore'
+import { usePaginationRequest } from '../../zustand/paginationStore'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  //useEffect that manage the blocking loading screen
-  //everytime the clildren changes, this makes sure we dont land in a loading screen
+  //* -------- Things to do when landing in a new page --------
+
+  // 1- everytime the clildren changes, this makes sure we dont land in a loading screen
   const get_isLoading = useLoading((state) => state.get_isLoading)
   const set_isLoading = useLoading((state) => state.set_isLoading)
 
+  // 2- resets pagination so next time we land in a component with a table, the table is in default order id desc
+  const setPaginationResquest = usePaginationRequest(
+    (state) => state.setPaginationRequest,
+  )
+
+  //do stuff!
   useEffect(() => {
     set_isLoading(false)
+    setPaginationResquest(null)
   }, [children])
 
+  //* -------- render! --------
   return (
     <>
       <div className="flex flex-col h-screen justify-between">
